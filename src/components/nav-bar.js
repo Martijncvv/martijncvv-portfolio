8c046ec25c4f82dbcce34ab6cf4afab1ff226f1d
Martijn <martijncvv@gmail.com>
2021-07-14 16:37:59 +0200

    setup for text content

---
+++ src/components/nav-bar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./nav-bar.css";
+
+function NavBar() {
+	return (
+		<div id="header">
+			<p>Martijn van Veen</p>
+		</div>
+	);
+}
+
+export default NavBar;
--- src/pages/draw-lorenz.js
+++ src/pages/draw-lorenz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Sketch from "react-p5";
 import "../styling/draw-lorenz.css";
+import NavBar from "../components/nav-bar";
 
 function DrawLorenz() {
 	const screenWidth = window.screen.width;
@@ -16,9 +17,16 @@ function DrawLorenz() {
 	let i = 0;
 
 	const dataPoints = [];
+	let sphere;
 
 	const setup = (p5) => {
-		p5.createCanvas(screenWidth, screenHeight * 0.9, p5.WEBGL);
+		const canvas = p5.createCanvas(screenWidth, screenHeight * 0.9, p5.WEBGL);
+		canvas.parent("sketch-holder");
+		// canvas.style("overflow", "hidden");
+	};
+
+	const testF = () => {
+		console.log("TESTTEST");
 	};
 
 	const draw = (p5) => {
@@ -26,9 +34,9 @@ function DrawLorenz() {
 		p5.rotateX(1.6);
 		p5.rotateZ(1.7);
 		p5.translate(0, -10, -350);
-		console.log(i);
+		// console.log(i);
 		i += 0.003;
-		p5.background(6, 41, 92);
+		p5.background(4, 30, 69);
 
 		let dt = 0.01;
 		let dx = a * (y - x) * dt;
@@ -38,30 +46,94 @@ function DrawLorenz() {
 		x += dx;
 		y += dy;
 		z += dz;
-		if (dataPoints.length < 50000) {
-			dataPoints.push({ x, y, z });
+
+		dataPoints.push({ x, y, z });
+		if (dataPoints.length > 1000) {
+			dataPoints.shift();
 		}
 
 		p5.scale(13);
-		p5.stroke(255);
+		p5.stroke(247, 247, 247);
 		p5.noFill();
 
 		p5.beginShape();
 		dataPoints.forEach((dataPoint) => {
+			// console.log(dataPoint);
 			p5.vertex(dataPoint.x, dataPoint.y, dataPoint.z);
+			if (
+				dataPoint.x === -8.04130748960242 &&
+				dataPoint.y === -8.651822589990463 &&
+				dataPoint.z === 28.27212631037705
+			) {
+				// p5.translate(dataPoint.x, dataPoint.y, dataPoint.z);
+				// sphere = p5.sphere(1);
+				// // console.log(p5.screenX())
+				// p5.translate(-dataPoint.x, -dataPoint.y, -dataPoint.z);
+			}
 		});
 		p5.endShape();
+
+		if (p5.pmouseX == -8.04130748960242 && p5.pmouseY == -8.651822589990463) {
+			testF();
+		}
+		// console.log(p5.pMouseX);
+
+		// console.log(sphere);
+		// console.log(p5.mouseY);
 	};
 
 	return (
 		<>
-			<div id="header">header</div>
-			<div id="sketch-div">
-				<Sketch setup={setup} draw={draw} />
-				<div id="overlay"></div>
-			</div>
+			<NavBar />
+			<Sketch setup={setup} draw={draw} />
+			<div id="sketch-holder"></div>
+			{/* <div id="overlay"></div> */}
 
-			<div></div>
+			<div id="content">
+				<div id="top-container">
+					<div id="top-container-left">
+						<div>
+							<h1>Martijn van Veen</h1>
+							<h2>Full Stack Developer</h2>
+						</div>
+
+						<button id="contact-button">Contact Me</button>
+					</div>
+
+					<div id="top-container-right">
+						<ul>
+							<li>INTP/INTF Logician/Mediator, </li>
+							<li>Humanitarian: Driven to make the world a better place. </li>
+							<li>
+								Creative and imaginative in coming up with insightful solutions
+								to meaningful problems.
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div id="mid-container">
+					<div id="mid-container-left">
+						<h1>About me</h1>
+					</div>
+					{/* <div id="mid-container-mid"></div> */}
+					<div id="mid-container-right">
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book.{" "}
+						</p>
+					</div>
+				</div>
+				<div id="bottom-container">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum has been the industry's standard dummy text
+						ever since the 1500s, when an unknown printer took a galley of type
+						and scrambled it to make a type specimen book.{" "}
+					</p>
+				</div>
+			</div>
 		</>
 	);
 }
