30105fad16866d51a27723b820d1d5f6f36cd879
Martijn <martijncvv@gmail.com>
2021-07-18 02:28:26 +0200

    changed styling to responsive style

--- src/pages/draw-lorenz.js
+++ src/pages/draw-lorenz.js
@@ -2,12 +2,15 @@ import React from "react";
 import Sketch from "react-p5";
 import "../styling/draw-lorenz.css";
 import NavBar from "../components/nav-bar";
-
-import TranspaDashboard from "../images/transpa/Transpa-page-dashboard.png";
-import cryptoExtension from "../images/cryptoextension/CG_twitter_extension_screenshot_ETH_USD_CROP.png";
-import Cryptofolio from "../images/cryptofolio/cryptofolio_home-page-view_btc.png";
-import chipsAndCircuitsWeb from "../images/chipsAndCircuits/chipsAndCircuitsWebV3.png";
 import Card from "../components/card";
+
+import transpaDashboard from "../images/transpa/Transpa-page-dashboard.png";
+import cryptoExtension from "../images/cryptoextension/CG_twitter_extension_screenshot_ETH_USD_CROP.png";
+import cryptofolio from "../images/cryptofolio/cryptofolio_home-page-view_btc.png";
+import chipsAndCircuitsWeb from "../images/chipsAndCircuits/chipsAndCircuitsWebV3.png";
+import githubLogo from "../images/socialMedia/githubLogo.png";
+import twitterLogo from "../images/socialMedia/twitterLogo.png";
+import linkedinLogo from "../images/socialMedia/linkedinLogo.png";
 
 function DrawLorenz() {
 	const screenWidth = window.screen.width;
@@ -27,7 +30,10 @@ function DrawLorenz() {
 	// const c = 8 / 3;
 
 	let i = 0;
-	let scale = 13;
+	// DESKTOP
+	// let scale = 13;
+	// MOBILE
+	let scale = 6;
 
 	const dataPoints = [];
 
@@ -36,31 +42,29 @@ function DrawLorenz() {
 		canvas.parent("sketch-holder");
 	};
 
-	const testF = () => {
-		console.log("TESTTEST");
-	};
 	document.addEventListener("DOMContentLoaded", function () {
-		let topContainer = document.getElementById("top-container");
-		let bottomContainer = document.getElementById("bottom-container");
-		let footerContainer = document.getElementById("footer-container");
-
-		topContainer.addEventListener("mouseenter", function (event) {
-			scale = 13;
-		});
-		bottomContainer.addEventListener("mouseenter", function (event) {
-			scale = 11;
-		});
-		footerContainer.addEventListener("mouseenter", function (event) {
-			scale = 11;
-		});
+		// let topContainer = document.getElementById("top-container");
+		// let bottomContainer = document.getElementById("bottom-container");
+		// let footerContainer = document.getElementById("footer-container");
+		// topContainer.addEventListener("mouseenter", function (event) {
+		// 	scale = 13;
+		// });
+		// bottomContainer.addEventListener("mouseenter", function (event) {
+		// 	scale = 11;
+		// });
+		// footerContainer.addEventListener("mouseenter", function (event) {
+		// 	scale = 11;
+		// });
 	});
 
 	const draw = (p5) => {
 		p5.rotateY(i);
 		p5.rotateX(1.6);
 		p5.rotateZ(1.7);
-		p5.translate(0, -10, -350);
-		console.log(i);
+
+		console.log("window.screen.height", window.screen.height);
+		p5.translate(0, 0, -(window.screen.height / 5));
+
 		i += 0.003;
 		p5.background(4, 30, 69);
 
@@ -91,90 +95,82 @@ function DrawLorenz() {
 
 	return (
 		<>
-			<NavBar />
 			<Sketch setup={setup} draw={draw} />
 			<div id="sketch-holder"></div>
 
-			<div id="content">
-				<div id="top-container">
-					<div id="top-container-left">
-						<div id="name">
-							<h1>Martijn van Veen</h1>
-							<h2>Full Stack Developer</h2>
-						</div>
-
-						<a href="#" target="_blank" id="contact-button">
+			<div className="main">
+				<NavBar />
+				<div
+					id="first-container"
+					className="container flex-wrap-between fc-100"
+				>
+					<div className="container-element">
+						<h1>Martijn van Veen</h1>
+						<h2>Full Stack Developer</h2>
+						<a href="#" target="_blank">
 							Contact me
 						</a>
 					</div>
 
-					<div id="top-container-right">
-						<ul>
-							<li>INTP/INTF Logician/Mediator, </li>
-							<li>Humanitarian: Driven to make the world a better place. </li>
-							<li>
-								Creative and imaginative in coming up with insightful solutions
-								to meaningful problems.
-							</li>
-						</ul>
+					<div className="container-element right-element">
+						<p>INTP/INTF Logician/Mediator,</p>
+						<p>Humanitarian: Driven to make the world a better place.</p>
+						<p>
+							Creative and imaginative in coming up with insightful solutions to
+							meaningful problems.
+						</p>
 					</div>
 				</div>
-				<div id="mid-container">
-					<div id="mid-container-left">
-						<div className="mid-container-text">
-							<h1>About</h1>
-							<p>
-								My name is Martijn van Veen, 26 years old and I am interested in
-								technological innovations. This ranges from blockchain
-								technology to artificial intelligence, and from apps that track
-								body data to the third industrial revolution.
-								<br /> <br /> In my free time I like to create (educational)
-								content on social media about various blockchain projects.
-								Currently I’m developing myself to be able to work as a
-								developer at an innovative and open-minded company.
-							</p>
-						</div>
+
+				<div className="container flex-wrap-between bgc-100 ">
+					<div className="container-element">
+						<h1>About</h1>
+						<p>
+							My name is Martijn van Veen, 26 years old and I am interested in
+							technological innovations. This ranges from blockchain technology
+							to artificial intelligence, and from apps that track body data to
+							the third industrial revolution.
+							<br /> <br /> In my free time I like to create (educational)
+							content on social media about various blockchain projects.
+							Currently I’m developing myself to be able to work as a developer
+							at an innovative and open-minded company.
+						</p>
 					</div>
 
-					<div id="mid-container-right">
-						<div className="mid-container-text">
-							<h1>Me</h1>
-							<div id="mid-container-right-content">
-								<div className="mid-container-right-box">
-									<ul>
-										<h4>Stacks</h4>
-										<li>Javascript</li>
-										<li>React</li>
-										<li>Redux</li>
-										<li>HTML</li>
-										<li>CSS</li>
-										<li>SQL</li>
-										<li>Python</li>
-									</ul>
-								</div>
-								<div className="mid-container-right-box">
-									<ul>
-										<h4>Hobbies</h4>
-										<li>Fitness</li>
-										<li>Reading</li>
-										<li>Travelling</li>
-										<li>Playing Guitar</li>
-									</ul>
-								</div>
-								<div className="mid-container-right-box">
-									<ul>
-										<h4>Interests</h4>
-										<li>Cryptocurrencies</li>
-										<li>Sustainability</li>
-										<li>Philosophy</li>
-									</ul>
-								</div>
+					<div className="container-element ">
+						<h1>Me</h1>
+						<div>
+							<div className="flex-wrap-between">
+								<ul>
+									<h3>Stacks</h3>
+									<li>Javascript</li>
+									<li>React</li>
+									<li>Redux</li>
+									<li>HTML</li>
+									<li>CSS</li>
+									<li>SQL</li>
+									<li>Python</li>
+								</ul>
+								<p></p>
+								<ul>
+									<h3>Hobbies</h3>
+									<li>Fitness</li>
+									<li>Reading</li>
+									<li>Travelling</li>
+									<li>Playing Guitar</li>
+								</ul>
+								<ul>
+									<h3>Interests</h3>
+									<li>Cryptocurrencies</li>
+									<li>Sustainability</li>
+									<li>Philosophy</li>
+								</ul>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div id="bottom-container">
-					<div id="bottom-container-left">
+				<div className="container">
+					<div>
 						<Card
 							title="Transpa Transparency Platform"
 							text="A platform where SMB's can create pages for products they sell
@@ -182,7 +178,7 @@ function DrawLorenz() {
 						customer by communicating through the product."
 							stacks="Javascript, React, Redux, Sequalize, HTML, CSS"
 							GHLink="https://github.com/Martijncvv/transpa_frontend"
-							background={TranspaDashboard}
+							background={transpaDashboard}
 						/>
 						<Card
 							title="Cryptocurrency Ticker Extension"
@@ -194,7 +190,7 @@ function DrawLorenz() {
 							background={cryptoExtension}
 						/>
 					</div>
-					<div id="bottom-container-right">
+					<div>
 						<Card
 							title="Cryptofolio"
 							text="A clean cryptocurrency portfolio tracker that shows (historical) price
@@ -202,7 +198,7 @@ function DrawLorenz() {
 						new investment opportunities within the cryptocurrency industry."
 							stacks="Python, Django, Javascript, HTML, CSS"
 							GHLink="https://github.com/Martijncvv/cryptocurrency_portfolio_project"
-							background={Cryptofolio}
+							background={cryptofolio}
 						/>
 						<Card
 							title="Chips and Circuits"
@@ -216,37 +212,36 @@ function DrawLorenz() {
 					</div>
 				</div>
 
-				<div id="footer-container">
-					<div id="footer-container-left">
-						<div className="footer-container-description">
-							<h1>Contact</h1>
+				<div className="container flex-wrap-between fc-100">
+					<div className="container-element">
+						<h1>Contact</h1>
+						<div id="contact">
 							<a href="https://github.com/Martijncvv" target="_blank">
-								Github
+								<img src={githubLogo} />
 							</a>
+
 							<a href="https://www.linkedin.com/in/martijncvv/" target="_blank">
-								Linkedin
+								<img src={linkedinLogo} />
 							</a>
+
 							<a href="https://twitter.com/Martijncvv" target="_blank">
-								Twitter
+								<img src={twitterLogo} />
 							</a>
 						</div>
 					</div>
 
-					<div id="footer-container-right">
-						<div className="footer-container-description">
-							<h1>Butterfly Effect</h1>
-							<p>
-								In chaos theory, the butterfly effect is the sensitive
-								dependence on initial conditions in which a small change in one
-								state of a deterministic nonlinear system can result in large
-								differences in a later state.
-							</p>
-							<p>
-								The term butterfly effect comes from an analogy where a
-								butterfly flaps its wings in Tokyo and a tornado occurs in
-								Tennessee.
-							</p>
-						</div>
+					<div className="container-element right-element">
+						<h2>Butterfly Effect</h2>
+						<p>
+							In chaos theory, the butterfly effect is the sensitive dependence
+							on initial conditions in which a small change in one state of a
+							deterministic nonlinear system can result in large differences in
+							a later state.
+						</p>
+						<p>
+							The term butterfly effect comes from an analogy where a butterfly
+							flaps its wings in Tokyo and a tornado occurs in Tennessee.
+						</p>
 					</div>
 				</div>
 			</div>
