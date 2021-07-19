ad839e5a3b90c2ee477bb2712c88c5726d2a911b
Martijn <martijncvv@gmail.com>
2021-07-19 14:49:35 +0200

    mobile styling screenheight

--- src/pages/draw-lorenz.js
+++ src/pages/draw-lorenz.js
@@ -39,7 +39,7 @@ function DrawLorenz() {
 	// MOBILE
 	if (screenWidth <= 600) {
 		scale = screenHeight / 160;
-		sketchPlacement = screenHeight / 150;
+		sketchPlacement = screenHeight / 100;
 		desktop = false;
 	}
 
@@ -74,7 +74,7 @@ function DrawLorenz() {
 
 		i += 0.003;
 		// p5.background(4, 30, 69);
-		p5.background(255, 30, 69);
+		p5.background(0, 30, 69);
 
 		let dt = 0.01;
 		let dx = a * (y - x) * dt;
